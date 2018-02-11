930bf508e77b8d7b22e9311e73435f8b83698cb4
Paul Houston <phouston1@email.arizona.edu>
2018-02-10 23:19:16 -0700

    Added get and set functions

--- projects/mivcots/CarData/CarData.h
+++ projects/mivcots/CarData/CarData.h
@@ -2,17 +2,13 @@
 
 #include <map>
 
-#define KEYLENGTH 2
+#define KEYLENGTH 3
 
 typedef struct dataValue_st {
 	union unprocessed {
 		long s;
 		unsigned long u;
-	};
-
-	union processed {
-		long i;
-		double d;
+		double d
 	};
 	
 } dataValue;
@@ -29,7 +25,13 @@ public:
 	CarData();
 	~CarData();
 
-	
+	int set(char* key, long newValue);
+	int set(char* key, unsigned long newValue);
+	int set(char* key, double newValue);
+
+	int get(char* key, long* dest);
+	int get(char* key, unsigned long* dest);
+	int get(char* key, double* dest);	
 
 protected:
 	std::map<char*, dataValue, cStrCmp> dataMap;
