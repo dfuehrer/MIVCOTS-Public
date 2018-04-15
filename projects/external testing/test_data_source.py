a93fc290aa2a3ca60ce0a73ad0a839668c1628ef
root <root@DESKTOP-IKEHOUK.localdomain>
2018-04-15 13:16:10 -0700

    changed to test data

--- projects/external testing/test_data_source.py
+++ projects/external testing/test_data_source.py
@@ -4,14 +4,14 @@ import time
 # Open the Serial Port
 ser = serial.Serial(port = "COM42", baudrate=115200)
 
-with open("November14Sorted.csv") as f:
+with open("testdata.csv") as f:
     data = f.readlines()
 
 
 while 1:
     for line in data:
-        print(line.replace("\n","") + ",!\n")
+        print("#," + line.replace("\n","") + ",!\n")
         line = line.replace("\n","") + ",!\n"
-        ser.write(line.encode("utf-8"))
+        ser.write("#," + line.encode("utf-8"))
         #ser.write(line.replace("\n","") + ",!\n")
         time.sleep(1)
