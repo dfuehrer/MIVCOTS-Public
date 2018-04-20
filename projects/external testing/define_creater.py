22a1370f4e49eddd439f49ffc40cbae54d825539
root <root@DESKTOP-IKEHOUK.localdomain>
2018-04-19 18:51:53 -0700

    modified key gen script to have a define with the numer of keys and added the header to the project

--- projects/external testing/define_creater.py
+++ projects/external testing/define_creater.py
@@ -3,8 +3,14 @@ with open("Keys_Define_Sheet.csv") as f:
 
 f = open('key_defines.h', 'w')
 
+defines = []
 for line in data:
     line = line.replace(",\n","").replace("\n","").split(',')
     if(len(line) == 2):
         print("#define " + line[1] + " \"" + line[0] + "\"")
-        f.write("#define " + line[1] + " \"" + line[0] + "\"\n")
+        defines.append("#define " + line[1] + " \"" + line[0] + "\"\n")
+        #f.write("#define " + line[1] + " \"" + line[0] + "\"\n")
+
+f.write("#define numKeys " + str(len(defines)) + "\n")
+for line in defines:
+    f.write(line)
