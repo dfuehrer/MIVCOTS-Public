07624090eb6e4636fe251c53a9758d482cd863ae
root <root@DESKTOP-IKEHOUK.localdomain>
2018-04-21 16:26:17 -0700

    made the key defines generation have more keys

--- projects/external testing/define_creater.py
+++ projects/external testing/define_creater.py
@@ -1,7 +1,7 @@
 with open("Keys_Define_Sheet.csv") as f:
     data = f.readlines()
 
-f = open('key_defines.h', 'w')
+f = open('../mivcots/key_defines.h', 'w')
 
 defines = []
 vectorList = []
@@ -9,8 +9,13 @@ for line in data:
     line = line.replace(",\n","").replace("\n","").split(',')
     if(len(line) == 2):
         print("#define " + line[1] + " \"" + line[0] + "\"")
-        defines.append("#define " + line[1] + " \"" + line[0] + "\"\n")
-        vectorList.append(line[1])
+        defines.append("#define " + line[1]+"_U" + " \"" + line[0] + "U\"\n")
+        defines.append("#define " + line[1]+"_D" + " \"" + line[0] + "D\"\n")
+        defines.append("#define " + line[1]+"_S" + " \"" + line[0] + "S\"\n")
+        vectorList.append(line[1] + "_U")
+        vectorList.append(line[1] + "_D")
+        vectorList.append(line[1] + "_S")
+
         #f.write("#define " + line[1] + " \"" + line[0] + "\"\n")
 
 #vectorList[-1] = vectorList[-1].replace(",", "")
@@ -23,7 +28,8 @@ f.write("#define numKeys " + str(len(defines)) + "\n")
 for line in defines:
     f.write(line)
 
-f.write("static std::map<std::string, std::string> keyMap {")
+f.write("static std::map<std::string, std::string> keyMap {\n")
 for keyID in vectorList[:-1]:
-	f.write("{" + keyID + ", \""  + keyID + "\"},")
+	f.write("{" + keyID + ", \""  + keyID + "\"},\n")
+
 f.write("{" + vectorList[-1] + ", \""  + vectorList[-1] + "\"}};")
--- projects/mivcots/key_defines.h
+++ projects/mivcots/key_defines.h
@@ -1,21 +1,101 @@
 #pragma once
 #include<map>
 #include<string>
-#define numKeys 16
-#define ID "AA"
-#define DATE "AB"
-#define TIME "AC"
-#define LON "AD"
-#define LAT "AE"
-#define ALTITUDE "AF"
-#define HEADING "AG"
-#define ACC_X "AH"
-#define ACC_Y "AI"
-#define ACC_Z "AJ"
-#define GYR_X "AK"
-#define GYR_Y "AL"
-#define GYR_Z "AM"
-#define MAG_X "AN"
-#define MAG_Y "AO"
-#define MAG_Z "AP"
-static std::map<std::string, std::string> keyMap {{ID, "ID"},{DATE, "DATE"},{TIME, "TIME"},{LON, "LON"},{LAT, "LAT"},{ALTITUDE, "ALTITUDE"},{HEADING, "HEADING"},{ACC_X, "ACC_X"},{ACC_Y, "ACC_Y"},{ACC_Z, "ACC_Z"},{GYR_X, "GYR_X"},{GYR_Y, "GYR_Y"},{GYR_Z, "GYR_Z"},{MAG_X, "MAG_X"},{MAG_Y, "MAG_Y"},{MAG_Z, "MAG_Z"}};
+#define numKeys 48
+#define ID_U "AAU"
+#define ID_D "AAD"
+#define ID_S "AAS"
+#define DATE_U "ABU"
+#define DATE_D "ABD"
+#define DATE_S "ABS"
+#define TIME_U "ACU"
+#define TIME_D "ACD"
+#define TIME_S "ACS"
+#define LON_U "ADU"
+#define LON_D "ADD"
+#define LON_S "ADS"
+#define LAT_U "AEU"
+#define LAT_D "AED"
+#define LAT_S "AES"
+#define ALTITUDE_U "AFU"
+#define ALTITUDE_D "AFD"
+#define ALTITUDE_S "AFS"
+#define HEADING_U "AGU"
+#define HEADING_D "AGD"
+#define HEADING_S "AGS"
+#define ACC_X_U "AHU"
+#define ACC_X_D "AHD"
+#define ACC_X_S "AHS"
+#define ACC_Y_U "AIU"
+#define ACC_Y_D "AID"
+#define ACC_Y_S "AIS"
+#define ACC_Z_U "AJU"
+#define ACC_Z_D "AJD"
+#define ACC_Z_S "AJS"
+#define GYR_X_U "AKU"
+#define GYR_X_D "AKD"
+#define GYR_X_S "AKS"
+#define GYR_Y_U "ALU"
+#define GYR_Y_D "ALD"
+#define GYR_Y_S "ALS"
+#define GYR_Z_U "AMU"
+#define GYR_Z_D "AMD"
+#define GYR_Z_S "AMS"
+#define MAG_X_U "ANU"
+#define MAG_X_D "AND"
+#define MAG_X_S "ANS"
+#define MAG_Y_U "AOU"
+#define MAG_Y_D "AOD"
+#define MAG_Y_S "AOS"
+#define MAG_Z_U "APU"
+#define MAG_Z_D "APD"
+#define MAG_Z_S "APS"
+static std::map<std::string, std::string> keyMap {
+{ID_U, "ID_U"},
+{ID_D, "ID_D"},
+{ID_S, "ID_S"},
+{DATE_U, "DATE_U"},
+{DATE_D, "DATE_D"},
+{DATE_S, "DATE_S"},
+{TIME_U, "TIME_U"},
+{TIME_D, "TIME_D"},
+{TIME_S, "TIME_S"},
+{LON_U, "LON_U"},
+{LON_D, "LON_D"},
+{LON_S, "LON_S"},
+{LAT_U, "LAT_U"},
+{LAT_D, "LAT_D"},
+{LAT_S, "LAT_S"},
+{ALTITUDE_U, "ALTITUDE_U"},
+{ALTITUDE_D, "ALTITUDE_D"},
+{ALTITUDE_S, "ALTITUDE_S"},
+{HEADING_U, "HEADING_U"},
+{HEADING_D, "HEADING_D"},
+{HEADING_S, "HEADING_S"},
+{ACC_X_U, "ACC_X_U"},
+{ACC_X_D, "ACC_X_D"},
+{ACC_X_S, "ACC_X_S"},
+{ACC_Y_U, "ACC_Y_U"},
+{ACC_Y_D, "ACC_Y_D"},
+{ACC_Y_S, "ACC_Y_S"},
+{ACC_Z_U, "ACC_Z_U"},
+{ACC_Z_D, "ACC_Z_D"},
+{ACC_Z_S, "ACC_Z_S"},
+{GYR_X_U, "GYR_X_U"},
+{GYR_X_D, "GYR_X_D"},
+{GYR_X_S, "GYR_X_S"},
+{GYR_Y_U, "GYR_Y_U"},
+{GYR_Y_D, "GYR_Y_D"},
+{GYR_Y_S, "GYR_Y_S"},
+{GYR_Z_U, "GYR_Z_U"},
+{GYR_Z_D, "GYR_Z_D"},
+{GYR_Z_S, "GYR_Z_S"},
+{MAG_X_U, "MAG_X_U"},
+{MAG_X_D, "MAG_X_D"},
+{MAG_X_S, "MAG_X_S"},
+{MAG_Y_U, "MAG_Y_U"},
+{MAG_Y_D, "MAG_Y_D"},
+{MAG_Y_S, "MAG_Y_S"},
+{MAG_Z_U, "MAG_Z_U"},
+{MAG_Z_D, "MAG_Z_D"},
+{MAG_Z_S, "MAG_Z_S"}};
